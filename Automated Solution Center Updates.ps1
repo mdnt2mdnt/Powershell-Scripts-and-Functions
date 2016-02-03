361258909493aa29f8ee6f93dc6e1432ac91ccb2
Phillip Marshall <phillip.r.marshall@outlook.com>
2016-02-03 15:45:15 -0500

    Initial commit of this file.
    Used to automation solution center updates on LabTech servers.

---
+++ Automated Solution Center Updates.ps1
@@ -0,0 +1,37 @@
+﻿#Function Declarations
+######################################
+function Log-Message ($msg)
+{
+	write-output $msg;
+	Add-Content $Logfile $msg;
+}
+
+#Prep Work 
+######################################
+
+$ScriptLog = "c:\windows\temp\script.txt"
+$UpdateLog = "c:\windows\temp\marketplaceupdates.txt"
+$ExistCheckSQLDir = CheckRegKeyExists HKLM:\Software\Wow6432Node\Labtech\Setup MySQLDir;
+$ExistCheckRootPwd = CheckRegKeyExists HKLM:\Software\Wow6432Node\Labtech\Setup RootPassword;
+$DownloadNeeded = $true;
+
+#Get the LT Share dir and set proper permissions
+################################################
+
+$32BitRegPath = 'HKLM:\Software\wow6432node\LabTech\Setup'
+$RegName = 'local ltshare'
+
+$LTShareDir = (Get-ItemProperty -Path $RegPath -Name $RegName).'Local LTShare'
+attrib -r $LTShareDir
+icacls $ltsharedir\* /T /Q /C /RESET
+Set-Location "C:\Program Files (x86)\LabTech Client"
+attrib -R *.* /S
+
+
+
+Set-Location "%programfiles32%\LabTech Client\"
+
+
+$args = @('/update', '/fix', '/commandfile C:\commandfile.txt')
+
+Start-Process -FilePath "%programfiles32%\LabTech Client\LTMarketplace.exe" -ArgumentList $Args
